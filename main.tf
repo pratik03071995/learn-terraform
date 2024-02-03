0400a636a6b87deca2542200b0ac3eecd0de55a1
pratik03071995 <108683914+pratik03071995@users.noreply.github.com>
2024-02-02 21:25:29 -0500

    update terraform code

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 provider "google" {
-  credentials = file("path/to/your/service-account-key.json")
+  credentials = file("numeric-chassis-412718-18373869df40.json")
   project     = "numeric-chassis-412718"
   region      = "us-central1"
 }
 
 resource "google_storage_bucket" "my_bucket" {
-  name   = "my-unique-bucket-name"
+  name   = "fnu-pratik-bucket-name"
   location = "US"
 }
 
 resource "google_bigquery_dataset" "my_dataset" {
-  dataset_id = "my_dataset"
+  dataset_id = "fnu_pratik_dataset"
 }
